745ea2c5afd57e93e95bae1b8eddd77910036473
Amy Collin <amyc271@gmail.com>
2017-10-25 14:36:22 +0100

    Worked on content on all pages and formating - no working JavaScript yet

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 document.getElementById('title').addEventListener('click', function () {
     console.log('Title was clicked');
 })
+
+$("#showAll").click(function() {
+    $(this).toggleClass("active");
+    $(".chairYoga").toggleClass("show");
+    $(".yoga").toggleClass("show");
+});
+
+$("#onlyChair").click(function() {
+    $(this).toggleClass("active");
+    $(".yoga").toggleClass("hide");
+});
+
+$("#onlyYoga").click(function() {
+    $(this).toggleClass("active");
+    $(".chairYoga").toggleClass("hide");
+});
