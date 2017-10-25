63d6137080cb32d9160517c1addd15e3035f5171
amysews <amyc271@gmail.com>
2017-10-25 22:26:15 +0100

    Got the JavaScript working for the class time table filtering

--- js/index.js
+++ js/index.js
@@ -1,38 +1,33 @@
+$("#all").click (function () {
+  $('.filterDiv').toggleClass("filterToggle", false);
+})
 
-filterSelection("all")
-function filterSelection(c) {
-    var x, i;
-    x = document.getElementsByClassName("filterDiv");
-    if (c == "all") c = "";
-    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class 
-    // from the elements that are not selected
-    for (i = 0; i < x.length; i++) {
-        w3RemoveClass(x[i], "show");
-        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-    }
-}
+$("#yoga").click (function () {
+  $('.chairYoga').toggleClass("filterToggle", true);
+  $('.yoga').toggleClass("filterToggle", false);
+})
 
-// Show filtered elements
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
+$("#chair").click (function () {
+  $('.chairYoga').toggleClass("filterToggle", false);
+  $('.yoga').toggleClass("filterToggle", true);
+})
 
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1); 
-    }
-  }
-  element.className = arr1.join(" ");
-}
+$("#randPoseGen").click (function () {
+  $('#targetNameEng') = "Downward Dog";
+  
+  // Generate random number between 1 and X (depending on how many poses I want to include)
+  //let randInt = ;
+
+  // Switch between 1 and X
+  //switch randInt:
+  //case 1:
+  //src="pose1.jpg";
+  //name="Pose 1";
+  //break;
+  //etc.
+
+  // Update or create for first time image according to src
+
+  // Update or create pose name next to image - english and sanscript 
+
+})
